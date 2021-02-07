7fa6d6cbe0a069aaee3fa0e71c7c3badf7ce2759
Pulpo-ita <75783797+Pulpo-ita@users.noreply.github.com>
2021-02-07 18:48:55 +0100

    add button

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var c = document.getElementById("fish");
-var ctx = c.getContext("2d");
+let c = document.getElementById("fish");
+let ctx = c.getContext("2d");
 
 const colors = ["FCF1DC","F50D03","A40000"]
 
-for(var i=0;i<100;i++){
-    var color = Math.floor(Math.random()*colors.length)
+for(let i=0;i<100;i++){
+    let color = Math.floor(Math.random()*colors.length)
     ctx.strokeStyle ="#" +  colors[color];
     ctx.beginPath();      
     ctx.arc(Math.floor(Math.random()*(800)+1) , Math.floor(Math.random()*(800)+1), Math.floor(Math.random()*(20)+1), 0 ,2*Math.PI); 
